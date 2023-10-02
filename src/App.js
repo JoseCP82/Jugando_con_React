a18735eb45b542392de80430b1eebd639c9ec048
JoseCP82 <josecamarap@gmail.com>
2023-10-02 18:49:39 +0200

    Nuevo contenido informativo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@
 function App() {
   return (
     <div className="App">
-      
+      <div>
+      <h2>
+        Estamos probando React
+      </h2>
+      </div>
+      <div>
+        <h3>
+          Nuevo contenido subido a GitHub
+        </h3>
+      </div>
     </div>
   );
 }
